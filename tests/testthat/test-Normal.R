af9f7ec262ecc6a2bff7dbb9f75402c52703c6ef
Moritz N. Lang <moritz.n.lang@gmail.com>
2022-01-04 14:58:21 +0100

    Implement tests (Binomial, Gamma), and small fixes

--- tests/testthat/test-Normal.R
+++ tests/testthat/test-Normal.R
@@ -80,43 +80,45 @@ test_that("{moments}.Normal work correctly", {
   expect_equal(kurtosis(n), 0)
 })
 
-test_that("vectorization of Normal work correctly", {
-  n <- Normal(c(0, 10), c(1, 1))
-  n1 <- n[1]
-  n2 <- n[2]
+  
 
-  expect_equal(mean(n), c(mean(n1), mean(n2)))
-  expect_equal(variance(n), c(variance(n1), variance(n2)))
-  expect_equal(skewness(n), c(skewness(n1), skewness(n2)))
-  expect_equal(kurtosis(n), c(kurtosis(n1), kurtosis(n2)))
+test_that("vectorization of a Normal distribution work correctly", {
+  d <- Normal(c(0, 10), c(1, 1))
+  d1 <- d[1]
+  d2 <- d[2]
 
-  set.seed(123); r1 <- random(n)
-  set.seed(123); r2 <- c(random(n1), random(n2))
+  expect_equal(mean(d), c(mean(d1), mean(d2)))
+  expect_equal(variance(d), c(variance(d1), variance(d2)))
+  expect_equal(skewness(d), c(skewness(d1), skewness(d2)))
+  expect_equal(kurtosis(d), c(kurtosis(d1), kurtosis(d2)))
+
+  set.seed(123); r1 <- random(d)
+  set.seed(123); r2 <- c(random(d1), random(d2))
   expect_equal(r1, r2)
 
-  expect_equal(pdf(n, 0), c(pdf(n1, 0), pdf(n2, 0)))
-  expect_equal(log_pdf(n, 0), c(log_pdf(n1, 0), log_pdf(n2, 0)))
-  expect_equal(cdf(n, 0.5), c(cdf(n1, 0.5), cdf(n2, 0.5)))
+  expect_equal(pdf(d, 0), c(pdf(d1, 0), pdf(d2, 0)))
+  expect_equal(log_pdf(d, 0), c(log_pdf(d1, 0), log_pdf(d2, 0)))
+  expect_equal(cdf(d, 0.5), c(cdf(d1, 0.5), cdf(d2, 0.5)))
 
-  expect_equal(quantile(n, 0.5), c(quantile(n1, 0.5), quantile(n2, 0.5)))
-  expect_equal(quantile(n, c(0.5, 0.5)), c(quantile(n1, 0.5), quantile(n2, 0.5)))
+  expect_equal(quantile(d, 0.5), c(quantile(d1, 0.5), quantile(d2, 0.5)))
+  expect_equal(quantile(d, c(0.5, 0.5)), c(quantile(d1, 0.5), quantile(d2, 0.5)))
   expect_equal(
-    quantile(n, c(0.1, 0.5, 0.9)), 
+    quantile(d, c(0.1, 0.5, 0.9)),
     matrix(
-      c(quantile(n1, c(0.1, 0.5, 0.9)), quantile(n2, c(0.1, 0.5, 0.9))), 
-      nrow = 2, 
-      ncol = 3, 
+      c(quantile(d1, c(0.1, 0.5, 0.9)), quantile(d2, c(0.1, 0.5, 0.9))),
+      nrow = 2,
+      ncol = 3,
       byrow = TRUE,
       dimnames = list(NULL, c("q_0.1", "q_0.5", "q_0.9"))
     )
   )
 
   expect_equal(
-    support(n), 
+    support(d),
     matrix(
-      c(support(n1), support(n2)), 
-      nrow = 2, 
-      ncol = 2, 
+      c(support(d1), support(d2)),
+      nrow = 2,
+      ncol = 2,
       byrow = TRUE,
       dimnames = list(NULL, c("min", "max"))
     )
