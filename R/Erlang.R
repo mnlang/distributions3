216d79733a4966a04e4667b957decba5aafa62a1
Moritz N. Lang <moritz.n.lang@gmail.com>
2022-01-05 21:23:53 +0100

    Start to update Erlang

--- R/Erlang.R
+++ R/Erlang.R
@@ -42,10 +42,6 @@ Erlang <- function(k, lambda) {
   d
 }
 
-rerlang <- function(n, k, lambda) {
-  -1/lambda * colSums(log(matrix(runif(n = n * k), ncol = n)))
-}
-
 #' Draw a random sample from an Erlang distribution
 #'
 #' @inherit Erlang examples
@@ -71,23 +67,24 @@ random.Erlang <- function(x, n = 1L, drop = TRUE, ...) {
 #' @param d An `Erlang` object created by a call to [Erlang()].
 #' @param x A vector of elements whose probabilities you would like to
 #'   determine given the distribution `d`.
+#' @param drop logical. Should the result be simplified to a vector if possible?
 #' @param ... Unused. Unevaluated arguments will generate a warning to
 #'   catch mispellings or other possible errors.
 #'
 #' @return A vector of probabilities, one for each element of `x`.
 #' @export
 #'
-pdf.Erlang <- function(d, x, ...) {
-  if (any(x < 0)) stop("'x' must be non-negative", call. = FALSE)
-  exp(log_pdf.Erlang(d, x, ...))
+pdf.Erlang <- function(d, x, drop = TRUE, ...) {
+  FUN <- function(at, d) derlang(x = at, k = d$k, lambda = d$lambda, ...)
+  apply_dpqr(d = d, FUN = FUN, at = x, type_prefix = "d", drop = drop)
 }
 
 #' @rdname pdf.Erlang
 #' @export
 #'
-log_pdf.Erlang <- function(d, x, ...) {
-  if (any(x < 0)) stop("'x' must be non-negative", call. = FALSE)
-  d$k * log(d$lambda) + (d$k - 1) * log(x) - d$lambda * x - lgamma(d$k)
+log_pdf.Erlang <- function(d, x, drop = TRUE, ...) {
+  FUN <- function(at, d) derlang(x = at, k = d$k, lambda = d$lambda, log = TRUE, ...)
+  apply_dpqr(d = d, FUN = FUN, at = x, type_prefix = "l", drop = drop)
 }
 
 #' Evaluate the cumulative distribution function of an Erlang distribution
@@ -97,21 +94,16 @@ log_pdf.Erlang <- function(d, x, ...) {
 #' @param d An `Erlang` object created by a call to [Erlang()].
 #' @param x A vector of elements whose cumulative probabilities you would
 #'   like to determine given the distribution `d`.
+#' @param drop logical. Should the result be simplified to a vector if possible?
 #' @param ... Unused. Unevaluated arguments will generate a warning to
 #'   catch mispellings or other possible errors.
 #'
 #' @return A vector of probabilities, one for each element of `x`.
 #' @export
 #'
-cdf.Erlang <- function(d, x, ...) {
-  if (any(x < 0)) stop("'x' must be non-negative", call. = FALSE)
-  internal <- Vectorize(FUN = function(d, x, ...) {
-    summation <- vector(mode = "numeric", length = d$k)
-    n <- 0:(d$k - 1)
-    summation <- 1 / factorial(n) * exp(-d$lambda * x) * (d$lambda * x)^n
-    return(1 - sum(summation))
-  }, vectorize.args = "x")
-  internal(d = d, x = x, ...)
+cdf.Erlang <- function(d, x, drop = TRUE, ...) {
+  FUN <- function(at, d) perlang(q = at, k = d$k, lambda = d$lambda, ...)
+  apply_dpqr(d = d, FUN = FUN, at = x, type_prefix = "p", drop = drop)
 }
 
 #' Determine quantiles of an Erlang distribution
@@ -122,32 +114,100 @@ cdf.Erlang <- function(d, x, ...) {
 #' @inheritParams random.Erlang
 #'
 #' @param probs A vector of probabilites.
+#' @param drop logical. Should the result be simplified to a vector if possible?
 #' @param ... Unused. Unevaluated arguments will generate a warning to
 #'   catch mispellings or other possible errors.
-#' @param interval Interval being used to search for the quantile using numerical root finding. Defaults to (0, 1e6)
-#' @param tol Tolerance of the root finding algorithm. Defaults to `.Machine$double.eps`
 #'
 #' @return A vector of quantiles, one for each element of `probs`.
 #' @export
 #'
-quantile.Erlang <- function(x, probs, ..., interval = c(0, 1e6), tol = .Machine$double.eps) {
+quantile.Erlang <- function(x, probs, drop = TRUE, ...) {
   ellipsis::check_dots_used()
-  if (any(probs < 0) | any(probs > 1)) stop("'probs' must be between 0 and 1.", call. = TRUE)
-  probs[probs == 1] <- (1 - .Machine$double.eps^0.25)
-  internal <- Vectorize(FUN = function(d, p, ..., interval, tol) {
-    qf <- function(x) cdf(d = d, x = x) - p
-    root <- stats::uniroot(qf, interval = interval, tol = tol, check.conv = TRUE)
-    return(root$root)
-  }, vectorize.args = "p")
-  internal(d = x, p = probs, ..., interval = interval, tol = tol)
+
+  FUN <- function(at, d) qerlang(at, k = d$k, lambda = d$lambda)
+  apply_dpqr(d = x, FUN = FUN, at = probs, type_prefix = "q", drop = drop)
 }
 
 #' Return the support of the Erlang distribution
 #'
 #' @param d An `Erlang` object created by a call to [Erlang()].
 #' @param drop logical. Should the result be simplified to a vector if possible?
+#' @param drop logical. Should the result be simplified to a vector if possible?
 #'
 #' @return A vector of length 2 with the minimum and maximum value of the support.
 #'
 #' @export
-support.Erlang <- function(d, drop = TRUE) c(0, Inf)
+support.Erlang <- function(d, drop = TRUE) {
+
+  stopifnot("d must be a supported distribution object" = is_distribution(d))
+  stopifnot(is.logical(drop))
+
+  min <- rep(0, length(d))
+  max <- rep(Inf, length(d))
+
+  make_support(min, max, drop = drop)
+}
+
+rerlang <- function(n, k, lambda) {
+  
+  stopifnot(is.numeric(n) && is.null(dim(n)))
+
+  par <- data.frame(k, lambda)
+
+  -1 / par$lambda * colSums(log(matrix(runif(n = n * par$k), ncol = n)))
+}
+
+derlang <- function(x, k, lambda, log = FALSE) {
+
+  stopifnot(all(x >= 0))
+
+  par <- data.frame(k, lambda)
+
+  rval <- par$k * log(par$lambda) + (par$k - 1) * log(x) - par$lambda * x - lgamma(par$k)
+
+  if (isTRUE(log)) {
+    rval
+  } else {
+    exp(rval)
+  }
+}
+
+perlang <- function(q, k, lambda, lower.tail = TRUE, log.p = FALSE) {
+
+  stopifnot(all(q >= 0))
+
+  # FIXME: lower.tail, log.p currently not supported
+  # FIXME: Simplify function
+
+  par <- data.frame(k, lambda)
+
+  internal <- Vectorize(FUN = function(par, q) {
+    summation <- vector(mode = "numeric", length = par$k)
+    n <- 0:(par$k - 1)
+    summation <- 1 / factorial(n) * exp(-par$lambda * q) * (par$lambda * q)^n
+    return(1 - sum(summation))
+  }, vectorize.args = "q")
+  internal(par = par, q = q)
+}
+
+qerlang <- function(p, k, lambda, lower.tail = TRUE, log.p = FALSE, 
+                    interval = c(0, 1e6), tol = .Machine$double.eps) {
+# interval Interval being used to search for the quantile using numerical root finding. Defaults to (0, 1e6)
+# tol Tolerance of the root finding algorithm. Defaults to `.Machine$double.eps`
+
+  stopifnot(all(p >= 0 & p <=1))
+
+  # FIXME: lower.tail, log.p currently not supported
+  # FIXME: Simplify function (remove interval and tol)
+
+  par <- data.frame(k, lambda)
+
+  p[p == 1] <- (1 - .Machine$double.eps^0.25)
+  internal <- Vectorize(FUN = function(par, p, interval, tol) {
+    qf <- function(x) perlang(q = x, k = par$k, lambda = par$lambda) - p
+    root <- stats::uniroot(qf, interval = interval, tol = tol, check.conv = TRUE)
+    return(root$root)
+  }, vectorize.args = "p")
+  internal(par = par, p = p, interval = interval, tol = tol)
+}
+
--- tests/testthat/test-Erlang.R
+++ tests/testthat/test-Erlang.R
@@ -48,3 +48,42 @@ test_that("quantile.Erlang works correctly", {
 test_that("support.Erlang works correctly", {
   expect_equal(support(e), c(0, Inf))
 })
+
+test_that("vectorization of a Erlang distribution work correctly", {
+  d <- Erlang(c(0.5, 0.8), 3)
+  d1 <- d[1]
+  d2 <- d[2]
+
+  set.seed(123); r1 <- random(d)
+  set.seed(123); r2 <- c(random(d1), random(d2))
+  expect_equal(r1, r2)
+
+  expect_equal(pdf(d, 0), c(pdf(d1, 0), pdf(d2, 0)))
+  expect_equal(log_pdf(d, 0), c(log_pdf(d1, 0), log_pdf(d2, 0)))
+  expect_equal(cdf(d, 0.5), c(cdf(d1, 0.5), cdf(d2, 0.5)))
+
+  expect_equal(quantile(d, 0.5), c(quantile(d1, 0.5), quantile(d2, 0.5)))
+  expect_equal(quantile(d, c(0.5, 0.5)), c(quantile(d1, 0.5), quantile(d2, 0.5)))
+  expect_equal(
+    quantile(d, c(0.1, 0.5, 0.9)),
+    matrix(
+      c(quantile(d1, c(0.1, 0.5, 0.9)), quantile(d2, c(0.1, 0.5, 0.9))),
+      nrow = 2,
+      ncol = 3,
+      byrow = TRUE,
+      dimnames = list(NULL, c("q_0.1", "q_0.5", "q_0.9"))
+    )
+  )
+
+  expect_equal(
+    support(d),
+    matrix(
+      c(support(d1), support(d2)),
+      nrow = 2,
+      ncol = 2,
+      byrow = TRUE,
+      dimnames = list(NULL, c("min", "max"))
+    )
+  )
+})
+
